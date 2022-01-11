bb0e7ff95c3200ab364391f255c460e19b802cf1
BossXI <bossweerawut@hotmail.com>
2022-01-11 17:18:27 +0700

    lab15_1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -2,9 +2,31 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[],int N){ 
+    int i,j; 
+	T key;
+    for (i = 1; i < N; i++)
+    { 
+        key = d[i]; 
+        j = i - 1; 
 
-}
+        while (j >= 0 && d[j] < key)
+        { 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = key;
+		cout << "Pass " << i << ":";
+
+		for(int i = 0 ; i < N; i++)
+		{
+			cout << d[i] << " ";
+
+		} 
+		cout << "\n";
+    } 
+
+} 
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
